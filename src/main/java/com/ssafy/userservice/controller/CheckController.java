1bfd6b7818fa8066b325bf38ddc5733f893f1ed5
오형택 <oh052679@gmail.com>
2024-05-11 14:54:07 +0900

    ✅Test :  add test api

--- src/main/java/com/ssafy/userservice/controller/CheckController.java
+++ src/main/java/com/ssafy/userservice/controller/CheckController.java
@@ -3,13 +3,11 @@ package com.ssafy.userservice.controller;
 import com.ssafy.userservice.dto.ChangeTimeZoneMessage;
 import com.ssafy.userservice.service.KafkaProducer;
 import jakarta.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,7 +19,9 @@ public class CheckController {
     private final KafkaProducer kafkaProducer;
 
     @Value("${kafka.topic.timezone-configured}")
-    private String topic;
+    private String tzTopic;
+    @Value("${kafka.topic.member-created}")
+    private String memberCreatedTopic;
 
     /**
      * 사용자 인증 후에만 접근 가능한 API 엔드포인트 sample access token 없이 혹은 유효하지 않은 access token 으로 접근하려면 에러 발생
@@ -46,10 +46,11 @@ public class CheckController {
     @GetMapping("/uncheck")
     public String uncheck(HttpServletRequest request) {
         log.info("Server port={}", request.getServerPort());
-        kafkaProducer.send(topic, ChangeTimeZoneMessage.builder()
+        kafkaProducer.send(tzTopic, ChangeTimeZoneMessage.builder()
             .memberId(1)
             .zoneId("Asia/Seoul")
             .build());
+        kafkaProducer.send(memberCreatedTopic, Map.of("id", 1, "email", "test@test.com"));
         return String.format("Hi, there. This is a message from First Service on PORT %s"
             , request.getServerPort());
     }
